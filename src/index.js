a6e62691abc6df27a9e650a9156d0f1d08f65336
Miguel <miguel.cuevas18@gmail.com>
2018-10-14 15:44:35 -0500

    created structure for project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.css';
-import App from './App';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
