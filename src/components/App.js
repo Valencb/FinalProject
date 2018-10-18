86806d2fd02ec4bec15f232f0a4bf48d3d901df7
Joel <joelchavoya.007@gmail.com>
2018-10-17 21:57:41 -0500

    Added SASS support and dependencies, started header

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './header/Header';
 import Main from './main/Main';
 import Footer from './footer/Footer';
+import '../css/main.css'
+import '../sass/main.scss'
 
 const App = () => (
       <div>
         <Header />
         <Main />
         <Footer />
-
       </div>
 )
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../css/main.css';
+import { Container, InputGroup, InputGroupAddon, Input, Row, Col, Button } from 'reactstrap'
 
 const Header = () => (
-    <div className="container-fluid">
-			<h1>Hello World, this is header</h1>
+    <div className="container-fluid header">
+        <Container>
+            <Row className="py-3 d-flex align-items-md-center justify-content-between">
+                <Col md="3">
+                        <h1>Logo</h1>
+                </Col>
+
+                <Col md="4">
+                    <InputGroup>
+                        <Input placeholder="Search our library..." />
+                        <InputG roupAddon addonType="append">@</InputGroupAddon>
+                    </InputGroup>
+                </Col>
+
+                <Col md="4" className="align-self-end">
+                    <Button className="hola" size="lg">Login or Register</Button>
+                </Col>
+            </Row>
+        </Container>
     </div>
 )
 
