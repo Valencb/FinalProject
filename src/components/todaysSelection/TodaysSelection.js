03805d5db6671320c58c3dc1d10ac212a80bfee3
Joel <joelchavoya.007@gmail.com>
2018-10-18 14:41:20 -0500

    Added today's selection

---
+++ src/components/todaysSelection/TodaysSelection.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+
+const TodaysSelection = () => (
+    <div className="todays-bgimage">
+        <div className="white-overlay">
+            <Row>
+                <Col md="12" className="text-center">
+                    <span className="h2">Today's selection</span>
+                    <br/>
+                    <span>Watch today's top rated films.</span>
+                </Col>
+            </Row>
+            <Row className="first-row">
+                <Col md="4">
+                    <Card>
+                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md="4">
+                    <Card>
+                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md="4">
+                    <Card>
+                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="second-row">
+                <Col md="4">
+                    <Card>
+                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md="4">
+                    <Card>
+                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </Col>
+                <Col md="4">
+                    <Card>
+                        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                        <CardBody>
+                            <CardTitle>Card title</CardTitle>
+                            <CardSubtitle>Card subtitle</CardSubtitle>
+                            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                            <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    </div>
+)
+
+export default TodaysSelection;
