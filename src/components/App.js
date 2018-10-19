b1910e7b8b54ef43ff37cd5529e2f34fc2435069
Joel <joelchavoya.007@gmail.com>
2018-10-18 21:22:37 -0500

    Edited privacy component and edited privacy component

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import Header from './header/Header';
 import LandingJumbotron from './landingJumbotron/LandingJumbotron'
 import TodaysSelection from './todaysSelection/TodaysSelection'
+import GetAPlan from './getAPlan/GetAPlan'
 import Privacy from './privacy/Privacy';
 import Footer from './footer/Footer';
 import '../sass/main.scss'
 
 const App = () => (
-      <div>
+    <div>
         <Header />
         <LandingJumbotron />
         <TodaysSelection/>
+        <GetAPlan/>
         <Privacy />
-        <Footer />
-      </div>
+        {/*<Footer />*/}
+    </div>
 )
 
 export default App;
