befa00f3fd757f0ef541b1e4acbe640c5afb2635
Valencb <valenciablancalu@gmail.com>
2018-10-19 19:26:51 -0500

    Ourplans frontend has been created

--- src/components/privacy/Privacy.js
+++ src/components/privacy/Privacy.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../../css/main.css';
+import { Row, Col, Button } from 'reactstrap';
 
 const Privacy = () => (
-    <div className="container-fluid" style={{border: '1px solid red'}}>
-        <div className='container d-flex'>
-            <div style={{border: '1px solid red'}} className='col'>
-                <img src={require('./../../images/qwerty.jpg')} alt="kk" style={{width: '100%'}}/>
-            </div>
-            <div style={{border: '1px solid red'}} className='col offset-1'>
-                hola2
-            </div>
-        </div>
-    </div>
+    <Row className="d-flex align-items-center privacy-main">
+        <Col md="6" className="">
+            <img src={require('../../img/5_tablet.jpg')} alt="kk" style={{width: '100%'}}/>
+        </Col>
+        <Col md="6" className="privacy-text">
+            <span className="h2">Worried about piracy?</span>
+            <br/><br/>
+            <p>
+                artLocus makes sure your videos are always safe using proprietary
+                security DRM software and recording detection algorithms.
+                We know how it feels to see your artwork being illegally distributed,
+                so we will take care of it as if it was ours.
+            </p>
+            <br/>
+            <hr className="privacy-hrline"/>
+            <p>
+                What security technologies does artLocus use?
+            </p>
+            <Button className="privacy-button">Find out</Button>
+        </Col>
+    </Row>
 )
 
 export default Privacy;
