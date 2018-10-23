6fc509290885e34ba34e6f42d2a81a2ee25ef6c4
Joel <joelchavoya.007@gmail.com>
2018-10-23 02:02:06 -0500

    Added modal and fixed header padding

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,107 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap'
+import { Container, Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter, Breadcrumb, BreadcrumbItem, Input, FormGroup, Label, Form} from 'reactstrap'
 
-const Header = () => (
-    <div className="container-fluid header py-3 sticky-top">
-        <Container className="align-items-md-center">
-            <Row className="py-3">
-                <Col>
-                        <h1 className="logotext">artLocus<span className="h6">®</span></h1>
-                </Col>
+class Header extends React.Component {
 
-                <Row className="align-items-md-center">
-                    <Col className="input-group align-items-md-center">
-                        <input type="text" className="form-control" placeholder="Search"/>
-                            <div className="input-group-append">
-                                <button className="btn searchbutton" type="submit"><span className="fas fa-search"></span></button>
-                            </div>
-                    </Col>
-                    <Col md="6 a">
-                        <Button className="loginbutton py-2" size="lg">Login or Register</Button>
-                    </Col>
-                </Row>
-            </Row>
-        </Container>
-    </div>
-)
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalToggle: false,
+            login: false,
+            register: true
+        };
+
+        this.toggle = this.toggle.bind(this);
+        this.loginToggle = this.loginToggle.bind(this);
+    }
+
+    toggle() {
+        this.setState({
+            modalToggle: !this.state.modalToggle
+        });
+    }
+
+    loginToggle() {
+        this.setState({login: !this.state.login})
+    }
+
+    render() {
+        return (
+            <div className="container-fluid header py-3 sticky-top">
+                <Container className="align-items-md-center">
+                    <Row className="py-1">
+                        <Col>
+                            <h1 className="logotext">artLocus<span className="h6">®</span></h1>
+                        </Col>
+
+                        <Row className="align-items-md-center">
+                            <Col className="input-group align-items-md-center">
+                                <input type="text" className="form-control" placeholder="Search"/>
+                                <div className="input-group-append">
+                                    <button className="btn searchbutton" type="submit"><span className="fas fa-search"></span></button>
+                                </div>
+                            </Col>
+                            <Col md="6 a">
+                                <Button className="loginbutton py-2" size="lg" onClick={this.toggle}>Login or Register</Button>
+                                    <Modal isOpen={this.state.modalToggle} toggle={this.toggle}>
+                                        <ModalHeader toggle={this.toggle}>
+                                            <Breadcrumb  onClick={this.loginToggle} >
+                                                <BreadcrumbItem active={!this.state.login}>
+                                                    <span className="unselectable">Login</span>
+                                                </BreadcrumbItem>
+                                                <BreadcrumbItem active={this.state.login}>
+                                                    <span className="unselectable">Register</span>
+                                                </BreadcrumbItem>
+                                            </Breadcrumb>
+                                        </ModalHeader>
+                                        <ModalBody>
+                                            {
+                                                this.state.login ?
+                                                    <Form>
+                                                        <span className="h5 text-center">Login to your account</span>
+                                                        <FormGroup className="mt-3">
+                                                            <Label for="userLogin">Username</Label>
+                                                            <Input type="text" name="userLogin" id="userLogin" placeholder="Username" />
+                                                        </FormGroup>
+                                                        <FormGroup>
+                                                            <Label for="passLogin">Password</Label>
+                                                            <Input type="password" name="passLogin" id="passLogin" placeholder="Password" />
+                                                        </FormGroup>
+                                                    </Form>
+                                                    :
+                                                    <Form>
+                                                        <span className="h5 text-center">Join artLocus today!</span>
+                                                        <FormGroup className="mt-3">
+                                                            <Label for="usernameReg">Choose a username</Label>
+                                                            <Input type="text" name="usernameReg" id="usrReg" placeholder="Write your username" />
+                                                        </FormGroup>
+                                                        <FormGroup>
+                                                            <Label for="mailReg">E-mail</Label>
+                                                            <Input type="email" name="mailReg" id="mailReg" placeholder="Enter a valid e-mail" />
+                                                        </FormGroup>
+                                                        <FormGroup>
+                                                            <Label for="passReg">Password</Label>
+                                                            <Input type="password" name="passReg" id="passReg" placeholder="Choose a strong password" />
+                                                        </FormGroup>
+                                                    </Form>
+                                            }
+                                        </ModalBody>
+                                        <ModalFooter>
+                                            {
+                                                this.state.login ?
+                                                    <Button className="loginbutton">Login</Button> :
+                                                    <Button className="loginbutton">Register</Button>
+                                            }
+                                            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                                        </ModalFooter>
+                                    </Modal>
+                            </Col>
+                        </Row>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
 
 export default Header;
--- src/components/ourPlans/Ourplans.js
+++ src/components/ourPlans/Ourplans.js
@@ -16,12 +16,12 @@ const Ourplans= () => (
                     <CardBody>
                     <CardTitle className="d-flex justify-content-center">Free</CardTitle>
                     <CardSubtitle className="d-flex justify-content-center mb-4">For viewers and art lovers</CardSubtitle>
-                    <CardText className="d-flex justify-content-center">
-                         <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Unlimited video views</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Video on demand access</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Rate your favorite videos</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Limited to one upload every 
+                    <CardText className="d-flex justify-content-center align-items-center">
+                         <ul className="fa-ul">
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Unlimited video views</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Video on demand access</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Rate your favorite videos</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Limited to one upload every 
                             two monts</li>
                         </ul>
                     </CardText>
@@ -33,11 +33,11 @@ const Ourplans= () => (
                     <CardTitle className="d-flex justify-content-center">Creative</CardTitle>
                     <CardSubtitle className="d-flex justify-content-center mb-4">For ambitious creative prodeucers and dreamers</CardSubtitle>
                     <CardText className="d-flex justify-content-center">
-                        <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>artLOcus demographic statistics</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Upload video on demand content</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>DRM managent</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Limited to one upload every month</li>
+                        <ul className="fa-ul">
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>artLOcus demographic statistics</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Upload video on demand content</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>DRM managent</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Limited to one upload every month</li>
                         </ul>
                     </CardText>
                     <Button className="bntregister mr-4">Subscribe</Button>
@@ -48,11 +48,11 @@ const Ourplans= () => (
                     <CardTitle className="d-flex justify-content-center">PRO</CardTitle>
                     <CardSubtitle className="d-flex justify-content-center mb-4">Company plans for the creative industry</CardSubtitle>
                     <CardText className="d-flex justify-content-center">
-                        <ul class="fa-ul">
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Subscription or monetization plans</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Promotion in the artLocus selection</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Parnership</li>
-                            <li><span class="fa-li"><i class="fa fa-check" aria-hidden="true"></i></span>Unlimited uploads</li>
+                        <ul className="fa-ul">
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Subscription or monetization plans</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Promotion in the artLocus selection</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Parnership</li>
+                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"></i></span>Unlimited uploads</li>
                         </ul>
                     </CardText>
                     <Button className="bntregister mr-4">Read more</Button>
