4350dfb534e29fe97ec0c7851cb55bf61baf1224
Valencb <valenciablancalu@gmail.com>
2018-11-05 11:45:23 -0600

    aligned buttons

--- Client/src/components/ourPlans/OurPlans.js
+++ Client/src/components/ourPlans/OurPlans.js
@@ -4,7 +4,6 @@ import { Row, Col, Card, Button, CardTitle, CardText, CardDeck, CardSubtitle, Ca
 const OurPlans = () => (
     <div className="ourplans1">
     <Row className="d-flex justify-content-center">
-        
         <div className="ourplans2 d-flex justify-content-center mb-4">
         <Col md="9">
             <span className="d-flex justify-content-center mt-3 h2">Our plans</span>
@@ -17,13 +16,13 @@ const OurPlans = () => (
                     <CardSubtitle className="d-flex justify-content-center mb-4">For viewers and art lovers</CardSubtitle>
                     <CardText className="d-flex justify-content-center align-items-center">
                          <ul className="fa-ul">
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Unlimited video views</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Video on-demand access</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Rate your favorite videos</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Limited to one upload every two months</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Unlimited video views</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Video on-demand access</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Rate your favorite videos</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Limited to one upload every two months</li>
                         </ul>
                     </CardText>
-                    <Button className="bntregister d-flex align-items-end">Register</Button>
+                    <Button className="bntplans-columns">Register</Button>
                     </CardBody>
                 </Card>
                 <Card className="content">
@@ -32,13 +31,13 @@ const OurPlans = () => (
                     <CardSubtitle className="d-flex justify-content-center mb-4">For ambitious creative producers and dreamers</CardSubtitle>
                     <CardText className="d-flex justify-content-center">
                         <ul className="fa-ul">
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>artLocus' demographic statistics</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Upload video on-demand content</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>DRM Management</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Limited to one upload every month</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>artLocus' demographic statistics</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Upload video on-demand content</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>DRM Management</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Limited to one upload every month</li>
                         </ul>
-                    </CardText>
-                    <Button className="bntregister mr-4">Subscribe</Button>
+                    </CardText> 
+                    <Button className="bntplans-columns">Subscribe</Button>
                     </CardBody>
                 </Card>
                    <Card className="content">
@@ -47,18 +46,18 @@ const OurPlans = () => (
                     <CardSubtitle className="d-flex justify-content-center mb-4">Company plans for the creative industry</CardSubtitle>
                     <CardText className="d-flex justify-content-center">
                         <ul className="fa-ul">
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Subscription as monetization plan</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Starred in artLocus' selection</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Partnership</li>
-                            <li><span className="fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Unlimited uploads</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Subscription as monetization plan</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Starred in artLocus' selection</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Partnership</li>
+                            <li><span className="li-ourplans fa-li"><i className="fa fa-check" aria-hidden="true"/></span>Unlimited uploads</li>
                         </ul>
                     </CardText>
-                    <Button className="bntregister mr-4">Read more</Button>
+                    <Button className="bntplans-columns">Read more</Button>
                     </CardBody>
                 </Card>
             </CardDeck>
             </div>
-            <span className="d-flex justify-content-center mt-3 mb-3"> Already a member?  <button className="ml-2 mb-3">Read more</button> </span>
+            <span className="d-flex justify-content-center mt-4 mb-3"> <p className="mt-2">Already a member?</p>  <Button className="bnt-upgrade ml-2 mb-4">Upgrade</Button> </span>
             </Col>
          </div >
     </Row>
